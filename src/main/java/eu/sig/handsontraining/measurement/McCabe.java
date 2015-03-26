a55a2fdc6d2d6c53928e51cd752c20489eb5144b
robvanderleek <robvanderleek@gmail.com>
2015-03-26 12:01:53 +0800

    Removed sysout.

--- src/main/java/eu/sig/handsontraining/measurement/McCabe.java
+++ src/main/java/eu/sig/handsontraining/measurement/McCabe.java
@@ -31,7 +31,6 @@ public class McCabe implements Measurement {
                 if (token.getType() == TokenTypes.QUESTION && previousTokenIsLessThan(tokens, token)) {
                     continue;
                 } else {
-                    System.out.println(token);
                     result++;
                 }
             }
